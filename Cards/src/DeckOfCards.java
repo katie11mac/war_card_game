d62591f6b2032dc5b3cd59c4d65d2397c9e4c9ca
katie <kmacalintal@middlebury.edu>
2021-08-17 23:16:29 -0400

    Comments added

--- Cards/src/DeckOfCards.java
+++ Cards/src/DeckOfCards.java
@@ -2,24 +2,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class DeckOfCards extends ArrayList<Card> implements Comparable<DeckOfCards>{
+public class DeckOfCards extends ArrayList<Card>{
 
+	
 	private ArrayList<Card> deck; // Size may change 
-	private int size; // key for comparing, may not be necessary 
+	
 	
 	//------------------------------------CONSTRUCTORS-----------------------------------------
-	
 	/*
-	 * Pass in a collection of Card objects (specialized deck of cards) 
+	 * Pass in a collection of Card objects (eg. specialized deck of cards) 
+	 * 
+	 * @param deck - Made deck of Card objects 
 	 */
 	public DeckOfCards(ArrayList<Card> deck) {
 		this.deck = deck; 
-		this.size = deck.size(); 
 	}
 	
 	
 	/*
-	 * Build a full deck of regular cards 
+	 * Build a full deck of regular cards.
 	 */
 	public DeckOfCards() {
 		String[] faceNames = Card.getValidFaceNames(); // static method (do not need an instance of a Card) 
@@ -32,129 +33,150 @@ public class DeckOfCards extends ArrayList<Card> implements Comparable<DeckOfCar
 				deck.add(new Card(faceName, suit));  
 			}
 		}
-		
-		this.size = deck.size(); 
-		
 	}
 	
-	//---------------------------------------SETTERS-------------------------------------------
 	
+	
+	//---------------------------------------SETTERS-------------------------------------------
+	/*
+	 * Set the deck to an already made deckOfCards
+	 * 
+	 * @param deck - collection of Card objects 
+	 */
 	public void setDeckOfCards(ArrayList<Card> deck) {
 		this.deck = deck; 
 	}
 	
-	public void setSize(int size) {
-		this.size = size; 
-	}
+	
 	
 	//---------------------------------------GETTERS-------------------------------------------
-
+	/*
+	 * Return deck of Card objects 
+	 * 
+	 * @return ArrayList<Card> - This collection of cards 
+	 */
 	public ArrayList<Card> getDeckOfCards() {
 		return this.deck; 
 	}
 	
-	public int getSize() {
-		return this.size;
-	}
 	
-	//-----------------------------------SORTING METHODS----------------------------------------
+	
+	// ------ IMPLEMENT SORTING METHODS HERE? ------ 
 	
 	
 	
 	//------------------------------------OTHER METHODS-----------------------------------------
-	
+	/*
+	 * Randomly rearrange the order of the Cards in the collection 
+	 */
 	public void shuffle() {
 		Collections.shuffle(this.deck);
 	}
 	
+	
+	/*
+	 * Add a Card object to the deck or collection of Card objects 
+	 * 
+	 * @param card - Card object to add to collection 
+	 */
 	public void addToDeck(Card card) {
 		this.deck.add(card); 
 	}
 	
+	
+	/*
+	 * Add a collection of Card objects to the deck 
+	 * 
+	 * @param cards - collection of Card objects to add to the collection 
+	 */
 	public void addToDeck(ArrayList<Card> cards) {
 		this.deck.addAll(cards); 
 	}
 	
+	
+	/*
+	 * Take the first Card object from the deck 
+	 * 
+	 * @return Card - first Card in the collection 
+	 */
 	public Card drawCard() {
 		return this.deck.remove(0); 
 	}
 	
-	public ArrayList<Card> drawCards(int numOfCards){
+	
+	/*
+	 * Take the first n number of Card objects from the deck 
+	 * 
+	 * @param n - number of Card objects to take from deck 
+	 * @return ArrayList<Card> - collection of the Card objects taken from the deck 
+	 */
+	public ArrayList<Card> drawCards(int n){
 		ArrayList<Card> cardsFromDeck = new ArrayList<Card>(); 
-		for (int i = 0; i < numOfCards; i++) {
+		for (int i = 0; i < n; i++) {
 			cardsFromDeck.add(this.deck.remove(0)); 
 		}
 		return cardsFromDeck; 
-		
 	}
-	//add to deck, single card parameter 
 	
-	// add to deck, collection of cards 
 	
-	// draw from deck, top card 
-	
-	// draw from deck, number of cards 
-	
-	//ArrayList<Card>[]
-	public ArrayList<Card>[] split(int numOfPiles){
-		if (this.size < numOfPiles)
-			throw new IllegalArgumentException("Not enough Cards in DeckOfCards to spilt into " + numOfPiles + " piles");
+	/*
+	 * Split the deck into n number of piles 
+	 * 
+	 * @param n - number of piles the deck of Card objects should be split into 
+	 * @return ArrayList<Card>[] - array of Card ArrayLists, 
+	 * 							   where each ArrayList represents a different pile of cards
+	 */
+	public ArrayList<Card>[] split(int n){
 		
-		ArrayList<Card>[] piles = new ArrayList[numOfPiles];
+		// Validity Check: Make sure there are enough cards to make the split 
+		if (deck.size() < n)
+			throw new IllegalArgumentException("Not enough Cards in DeckOfCards to spilt into " + n + " piles");
 		
 		
-		//need to also account for uneven splits 
-		int pileSize = this.size / numOfPiles; 
-		int remainder = this.size % numOfPiles; 
+		ArrayList<Card>[] piles = new ArrayList[n];
+		
+		
+		int pileSize = deck.size() / n; 
+		int remainder = deck.size() % n; // Need to account for uneven splits 
 		
 		
 		// This may be really slow, try to find a faster way soon 
 		
-		int cardIndex = 0; //keeps track of what card in the deck you are on 0 to 51 
+		int cardIndex = 0; //Tracking what card in the deck you are on (0 to 51) 
 		
-		for(int pilesMade = 0; pilesMade < numOfPiles; pilesMade++) {
+		for(int pilesMade = 0; pilesMade < n; pilesMade++) {
 			piles[pilesMade] = new ArrayList<Card>(); 
 			
-			if(pilesMade == numOfPiles - 1) {
-				for(int c = 0; c < pileSize + remainder; c++) {//think of c as the number of cards added to the pile
-					
+			if(pilesMade == n - 1) {
+				for(int c = 0; c < pileSize + remainder; c++) {
+					//c: number of cards added to the pile
 					piles[pilesMade].add(c, deck.remove(0)); 
 					cardIndex++; 
 				}
-			} else {//last pile being made; want to add the rest of the cards to the pile 
-				for(int c = 0; c < pileSize; c++) {//think of c as the number of cards added to the pile
+			} else { //last pile being made; want to add the rest of the cards to the pile 
+				for(int c = 0; c < pileSize; c++) {
+					//c: number of cards added to the pile
 					piles[pilesMade].add(c, deck.remove(0)); 
 					cardIndex++; 
 				}
 			}
 			
-			//System.out.println(piles[pilesMade]);
-			//System.out.println(piles[pilesMade].size()); 
 		}
 		
-//		for(int i = 0; i < piles[0].size(); i++) { 
-//			  System.out.println(piles[0].get(i)); 
-//		}
-//		
 		return piles; 
 	}
 	
+	/*
+	 * Return information of the deckOfCards object in a clean format 
+	 * 
+	 * @return String - Every faceName and corresponding suit of each Card object 
+	 */
 	public String toString() {
 		String deckString = ""; 
 		for (Card card: deck) {
 			deckString += card.toString() + " \n"; 
 		}
 		return deckString; 
-	}
-	
-	//honestly this might not be necessary 
-	public int compareTo(DeckOfCards other) {
-		if (this.size == other.size) 
-			return 0; 
-		else if (this.size < other.size)
-			return -1; //any neg 
-		else // if this > other 
-			return 1; //any pos 	
 	}
 	
 }
